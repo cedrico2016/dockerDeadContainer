fbad0be88383a27a2b1111edb861a968a2fb87c3
cedrico2016 <cedrico2015@gmail.com>
2024-02-20 16:59:53 +0100

    Worker Node

--- run_script.sh
+++ run_script.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mkdir /home/cedrico2015/kubernertesMaster
-cd /home/cedrico2015/kubernertesMaster
-
-wget https://dl.k8s.io/v1.24.1/kubernetes-server-linux-amd64.tar.gz
-tar -xzvf kubernetes-server-linux-amd64.tar.gz
-cd /home/cedrico2015/kubernertesMaster/kubernetes/server/bin/
-wget https://github.com/etcd-io/etcd/releases/download/v3.5.4/etcd-v3.5.4-linux-amd64.tar.gz
-tar -xzvf etcd-v3.5.4-linux-amd64.tar.gz
+mkdir ~/kubernertesWorker
+cd ~/kubernertesWorker
+wget https://dl.k8s.io/v1.24.1/kubernetes-node-linux-amd64.tar.gz
+tar -xzvf kubernetes-node-linux-amd64.tar.gz
